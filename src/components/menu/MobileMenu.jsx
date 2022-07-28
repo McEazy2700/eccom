317d10080ada5a77ee17f0839e1027e0602e8679
Ezekiel Victor <102771161+McEazy2700@users.noreply.github.com>
2022-07-28 19:12:53 +0100

    Chnged nav

--- src/components/menu/MobileMenu.jsx
+++ src/components/menu/MobileMenu.jsx
@@ -11,14 +11,14 @@ const MobileMenu = () => {
             <span htmlFor="dropdown" id="checked"><FaAngleRight /></span>
             <span htmlFor="dropdown" id='unchecked'><FaAngleDown /></span>
             <div className="dropdown__menu-content slide-in-right">
-                <a href="#">Top</a>
+                <a href="/">Top</a>
                 <a href="#products">Featured</a>
                 <a href="#about">About</a>
                 <a href="#contact">Contact</a>
             </div>
         </div>
         <div><Link to="/store">Store</Link></div>
-        <div><Link to="/about">About us</Link></div>
+        {/* <div><Link to="/about">About us</Link></div> */}
         <div><Link to="/contact">Contact us</Link></div>
         </>
     )
